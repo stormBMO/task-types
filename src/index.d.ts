3cdbe853803d952cb54fa4992a24238fe16ddd1a
stormBMO <andrey.chet76@gmail.com>
2022-11-20 17:58:54 +0300

    finish task

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,9 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+type TObj = { name: string; age: number; gender: 'male' | 'female' };
+type TArr = TObj[];
+export function getPersons(): TArr;
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +20,10 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+type TPerson = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+    company?: string;
+};
+export function personToString(person: TPerson): string;
